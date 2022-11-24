4489589519e70869bb4af31bd631a8444f73c2ef
DataLinh <84648948+DataLinh@users.noreply.github.com>
2022-11-24 23:31:51 +0700

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -32,5 +32,33 @@ CREATE TABLE `Users`  (
   CONSTRAINT `fk_Users_Nganh` FOREIGN KEY (`maNganh`) REFERENCES `Nganh` (`manganh`) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
 
+CREATE TABLE `HoiDong`  (
+  `id` varchar(255) NOT NULL,
+  `giaoVien1` varchar(255) NULL,
+  `giaoVien2` varchar(255) NULL,
+  `giaoVien3` varchar(255) NULL,
+  `giaoVien4` varchar(255) NULL,
+  `giaoVien5` varchar(255) NULL,
+	FOREIGN KEY (giaoVien1) REFERENCES Users(id),
+	FOREIGN KEY (giaoVien2) REFERENCES Users(id),
+	FOREIGN KEY (giaoVien3) REFERENCES Users(id),
+	FOREIGN KEY (giaoVien4) REFERENCES Users(id),
+	FOREIGN KEY (giaoVien5) REFERENCES Users(id),
+  PRIMARY KEY (`id`)
+);
 
+CREATE TABLE `DeTai` (
+  `maDeTai` varchar(255) NOT NULL,
+  `loaiDeTai` varchar(255) NULL,
+  `maNganh` varchar(255) NULL,
+  `gvHuongDan` varchar(255) NULL,
+  `gvPhanBien` varchar(255) NULL,
+  `hoiDongPhanBien` varchar(255) NULL,
+	FOREIGN KEY (loaiDeTai) REFERENCES LoaiDeTai(maLoaiDeTai),
+	FOREIGN KEY (maNganh) REFERENCES Nganh(maNganh),
+	FOREIGN KEY (gvHuongDan) REFERENCES Users(id),
+	FOREIGN KEY (gvPhanBien) REFERENCES Users(id),
+	FOREIGN KEY (hoiDongPhanBien) REFERENCES HoiDong(id),
+  PRIMARY KEY (`maDeTai`)
+);
 
