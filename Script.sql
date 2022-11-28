a7f202da6f9b1396f0eef955124abf02d639224e
DataLinh <84648948+DataLinh@users.noreply.github.com>
2022-11-28 08:03:13 +0700

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80013
  File Encoding         : 65001
 
- Date: 27/11/2022 18:54:29
+ Date: 28/11/2022 08:01:52
 */
 
 SET NAMES utf8mb4;
@@ -28,48 +28,46 @@ CREATE TABLE `DeTai`  (
   `gvHuongDan` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
   `gvPhanBien` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
   `maSinhVien` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `maHoiDong` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
   PRIMARY KEY (`maDeTai`) USING BTREE,
   INDEX `loaiDeTai`(`loaiDeTai`) USING BTREE,
   INDEX `maNganh`(`maNganh`) USING BTREE,
   INDEX `gvHuongDan`(`gvHuongDan`) USING BTREE,
   INDEX `gvPhanBien`(`gvPhanBien`) USING BTREE,
+  INDEX `maHoiDong`(`maHoiDong`) USING BTREE,
   INDEX `maDeTai`(`maDeTai`) USING BTREE,
   INDEX `maDeTai_2`(`maDeTai`) USING BTREE,
+  INDEX `DeTai_ibfk_5`(`maSinhVien`) USING BTREE,
   CONSTRAINT `DeTai_ibfk_1` FOREIGN KEY (`loaiDeTai`) REFERENCES `LoaiDeTai` (`maloaidetai`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `DeTai_ibfk_2` FOREIGN KEY (`maNganh`) REFERENCES `Nganh` (`manganh`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-	  CONSTRAINT `DeTai_ibfk_5` FOREIGN KEY (`maSinhVien`) REFERENCES `Users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-
   CONSTRAINT `DeTai_ibfk_3` FOREIGN KEY (`gvHuongDan`) REFERENCES `Users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `DeTai_ibfk_4` FOREIGN KEY (`gvPhanBien`) REFERENCES `Users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+  CONSTRAINT `DeTai_ibfk_4` FOREIGN KEY (`gvPhanBien`) REFERENCES `Users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `DeTai_ibfk_5` FOREIGN KEY (`maSinhVien`) REFERENCES `Users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `DeTai_ibfk_6` FOREIGN KEY (`maHoiDong`) REFERENCES `HoiDong` (`mahoidong`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of DeTai
 -- ----------------------------
-INSERT INTO `DeTai` VALUES ('DT001', 'DACNTT', 'CNTT', '2', '3', '4');
-INSERT INTO `DeTai` VALUES ('DT002', 'KHKT', 'CNTT', '3', '2', '4');
+INSERT INTO `DeTai` VALUES ('DT001', 'DACNTT', 'CNTT', '2', '3', '4', 'HD2022');
+INSERT INTO `DeTai` VALUES ('DT002', 'KHKT', 'CNTT', '3', '2', '4', 'HD2022');
 
 -- ----------------------------
 -- Table structure for HoiDong
 -- ----------------------------
 DROP TABLE IF EXISTS `HoiDong`;
 CREATE TABLE `HoiDong`  (
-  `maDeTai` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `maGiangVien` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`maGiangVien`, `maDeTai`) USING BTREE,
-  INDEX `maDeTai`(`maDeTai`) USING BTREE,
-  CONSTRAINT `HoiDong_ibfk_1` FOREIGN KEY (`maGiangVien`) REFERENCES `Users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `HoiDong_ibfk_2` FOREIGN KEY (`maDeTai`) REFERENCES `DeTai` (`madetai`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+  `maHoiDong` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `tenHoiDong` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`maHoiDong`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of HoiDong
 -- ----------------------------
-INSERT INTO `HoiDong` VALUES ('DT001', '1');
-INSERT INTO `HoiDong` VALUES ('DT001', '2');
-INSERT INTO `HoiDong` VALUES ('DT001', '3');
-INSERT INTO `HoiDong` VALUES ('DT002', '2');
-INSERT INTO `HoiDong` VALUES ('DT002', '3');
+INSERT INTO `HoiDong` VALUES ('HD2022', 'Hội đồng chấm đồ án 2022');
+INSERT INTO `HoiDong` VALUES ('HDECO', 'HOI DONG KINH TE');
+INSERT INTO `HoiDong` VALUES ('HDKH', 'HD CHẤM NCKH');
 
 -- ----------------------------
 -- Table structure for LoaiDeTai
@@ -82,7 +80,7 @@ CREATE TABLE `LoaiDeTai`  (
   `nam` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
   PRIMARY KEY (`maLoaiDeTai`) USING BTREE,
   INDEX `maLoaiDeTai`(`maLoaiDeTai`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of LoaiDeTai
@@ -101,7 +99,7 @@ CREATE TABLE `Nganh`  (
   PRIMARY KEY (`maNganh`) USING BTREE,
   INDEX `maNganh`(`maNganh`) USING BTREE,
   INDEX `maNganh_2`(`maNganh`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of Nganh
@@ -126,20 +124,24 @@ CREATE TABLE `Users`  (
   `schoolyear` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
   `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
   `role` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `maHoiDong` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
   PRIMARY KEY (`Id`) USING BTREE,
   INDEX `fk_Users_Nganh`(`maNganh`) USING BTREE,
   INDEX `Id`(`Id`) USING BTREE,
   INDEX `Id_2`(`Id`) USING BTREE,
   INDEX `Id_3`(`Id`) USING BTREE,
+  INDEX `fk_Users_HoiDong`(`maHoiDong`) USING BTREE,
+  CONSTRAINT `fk_Users_HoiDong` FOREIGN KEY (`maHoiDong`) REFERENCES `HoiDong` (`mahoidong`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `fk_Users_Nganh` FOREIGN KEY (`maNganh`) REFERENCES `Nganh` (`manganh`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of Users
 -- ----------------------------
-INSERT INTO `Users` VALUES ('1', '0', '1', 'Admin', NULL, NULL, NULL, NULL, 'Admin');
-INSERT INTO `Users` VALUES ('2', 'CNTT', '2', 'Trưởng bộ môn CNTT', NULL, NULL, NULL, NULL, 'TruongBoMon');
-INSERT INTO `Users` VALUES ('3', 'CNTT', '3', 'Giảng Viên', NULL, NULL, NULL, NULL, 'GiangVien');
-INSERT INTO `Users` VALUES ('4', 'CNTT', '4', 'Sinh Viên', NULL, NULL, NULL, NULL, 'SinhVien');
+INSERT INTO `Users` VALUES ('1', '0', '1', 'Admin', NULL, NULL, NULL, NULL, 'Admin', '0', NULL);
+INSERT INTO `Users` VALUES ('2', 'CNTT', '2', 'Trưởng bộ môn CNTT', NULL, NULL, NULL, NULL, 'TruongBoMon', 'HD2022', NULL);
+INSERT INTO `Users` VALUES ('3', 'CNTT', '3', 'Giảng Viên', NULL, NULL, NULL, NULL, 'GiangVien', 'HD2022', NULL);
+INSERT INTO `Users` VALUES ('4', 'CNTT', '4', 'Sinh Viên', NULL, NULL, NULL, NULL, 'SinhVien', '0', NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
